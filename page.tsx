3423399acb496b444a48199cda59f6392d02bcef
ECampbell37 <93222802+ECampbell37@users.noreply.github.com>
2024-10-04 22:24:21 -0400

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,82 +1,202 @@
-import Link from 'next/link'
+'use client'
+import { useState } from 'react'
+import { GithubIcon, LinkedinIcon } from 'lucide-react'
 import { Button } from "@/components/ui/button"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import Link from 'next/link'
 
-export default function SkillsPage() {
-  const skills = {
-    'AI/ML': {
-      description: 'My particular specialty and field of interest. I have taken college courses, Coursera Specialization online courses, and have worked on my own personal projects in the world of Machine Learning and AI. I have experience with Neural Networks, Machine Learning Algorithms such as Logistic Regression and Random Forests, Data Preprocessing, adjusting ML model parameters, and effectively utilizing generative AI technology. This field particularly excites me, and I hope to expand the world of AI/ML applications for many years to come.',
-      skillsList: [
-        { name: 'Python', logo: '/images/python.png' },
-        { name: 'TensorFlow', logo: '/images/tensorflow.png' },
-        { name: 'NumPy', logo: '/images/NumPy.png' },
-        { name: 'Jupyter Notebook', logo: '/images/Jupyter.png' },
-        { name: 'OpenAI', logo: '/images/openai.png' },
-      ],
-    },
-    'Web Application Development': {
-      description: 'Intermediate-level experience building web applications using Javascript, HTML, and CSS. I am familar working with the Javascript framework Next.js, and subsequently React. I have a good understanding of the fundamentals of web design and internet protocol. Web Applications are a great way to make your code accessible and easy to use for a wide range of users, and for that I appreicate their immense potential value.',
-      skillsList: [
-        { name: 'JavaScript', logo: '/images/js3.png' },
-        { name: 'React', logo: '/images/React.png' }, 
-        { name: 'Next.js', logo: '/images/nextjs.png' }, 
-        { name: 'CSS', logo: '/images/CSS.png' },
-        { name: 'HTML', logo: '/images/HTML.png' },
-      ],
-    },
-    'CS Fundamentals': {
-      description: 'Solid understanding of computer science principles and basic algorithms. Much of my college coursework was geared towards understanding the basics of computer architecture, data structures, algorithms, object-oriented language properties such as classes and polymorphism, and proper documentation/coding standards. I have a well rounded background in C/C++, and I understand the principles of low and high level coding languages.',
-      skillsList: [
-        { name: 'C', logo: '/images/C.png' },
-        { name: 'C++', logo: '/images/C++.png' }, 
-        { name: 'Visual Studio Code', logo: '/images/vscode.png' }, 
-      ],
-    },
-  }
+// Images array for scrolling images
+const images = [
+  '/images/C++.png',
+  '/images/CSS.png',
+  '/images/HTML.png',
+  '/images/js3.png',
+  '/images/react.png',
+  '/images/nextjs.png',
+  '/images/Jupyter.png',
+  '/images/NumPy.png',
+  '/images/openai.png',
+  '/images/python.png',
+  '/images/tensorflow.png',
+  '/images/Vscode.png'
+]
+
+export default function Home() {
+  const [activeTab, setActiveTab] = useState('Home')
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <div className="min-h-screen bg-white text-gray-900">
-      <header className="p-1 max-w-7xl mx-auto">
-        <div className="flex justify-end mt-4">
-          <Link href="/">
-            <Button variant="link">Back to Home</Button>
-          </Link>
-        </div>
-        <h1 className="mr-2 ml-2 text-4xl font-bold">Skills</h1>
-        <p className="mr-2 ml-2 mt-4 text-gray-700">
-          Here are some of the technologies and platforms I am skilled in. Each section describes my interest and background in the respective field, as well as how those skills were generally aquired. Check out my projects page and resume to learn more!
-        </p>
-        <hr className="my-6 border-gray-300" />
+      <header className="relative flex justify-between items-center p-4 max-w-7xl mx-auto">
+        <h1 className="text-xl font-semibold text-gray-900">Welcome!</h1>
+
+        {/* Mobile Menu Button */}
+        <button
+          className="absolute top-4 right-4 text-gray-900 md:hidden flex items-center"
+          onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+          title="Toggle mobile menu"
+        >
+          <span className="mr-2">Menu</span> {/* Added Menu text */}
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+          
+        </button>
+
+        <nav>
+          {/* Desktop Menu */}
+          <ul className="hidden md:flex items-center space-x-4">
+            <li>
+              <Link
+                href="/"
+                className={`${activeTab === 'Home' ? 'text-blue-600' : 'text-gray-600'} hover:text-blue-600`}
+                onClick={() => setActiveTab('Home')}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/skills"
+                className={`${activeTab === 'Skills' ? 'text-blue-600' : 'text-gray-600'} hover:text-blue-600`}
+                onClick={() => setActiveTab('Skills')}
+              >
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/projects"
+                className={`${activeTab === 'Projects' ? 'text-blue-600' : 'text-gray-600'} hover:text-blue-600`}
+                onClick={() => setActiveTab('Projects')}
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <a href="mailto:elijahcampbellihim@gmail.com" target="_blank" rel="noopener noreferrer">
+                <Button variant="default" className="h-auto px-4 py-2">
+                  Contact Me
+                </Button>
+              </a>
+            </li>
+          </ul>
+
+          {/* Mobile Menu */}
+          {isMobileMenuOpen && (
+            <ul className="absolute top-16 right-4 bg-white border border-gray-200 shadow-md rounded-lg p-4 space-y-4 md:hidden w-48">
+              <li>
+                <Link
+                  href="/"
+                  className={`${activeTab === 'Home' ? 'text-blue-600' : 'text-gray-600'} hover:text-blue-600`}
+                  onClick={() => {
+                    setActiveTab('Home')
+                    setMobileMenuOpen(false)
+                  }}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/skills"
+                  className={`${activeTab === 'Skills' ? 'text-blue-600' : 'text-gray-600'} hover:text-blue-600`}
+                  onClick={() => {
+                    setActiveTab('Skills')
+                    setMobileMenuOpen(false)
+                  }}
+                >
+                  Skills
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/projects"
+                  className={`${activeTab === 'Projects' ? 'text-blue-600' : 'text-gray-600'} hover:text-blue-600`}
+                  onClick={() => {
+                    setActiveTab('Projects')
+                    setMobileMenuOpen(false)
+                  }}
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <a href="mailto:elijahcampbellihim@gmail.com" target="_blank" rel="noopener noreferrer">
+                  <Button variant="default">Contact Me</Button>
+                </a>
+              </li>
+            </ul>
+          )}
+        </nav>
       </header>
 
-      <main className="max-w-7xl mx-auto p-4">
-        {/* Loop through skill groups */}
-        {Object.entries(skills).map(([category, { description, skillsList }]) => (
-          <div key={category} className="mb-16">
-            <h2 className="text-2xl font-semibold mt-4">{category}</h2>
-            <p className="mt-2 text-gray-700">{description}</p>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-8 mt-4">
-              {/* Loop through individual skills */}
-              {skillsList.map(skill => (
-                <div key={skill.name} className="p-5 bg-blue-100 rounded-md text-center">
-                  <img
-                    src={skill.logo}
-                    alt={`${skill.name} Logo`}
-                    className="w-16 h-16 mx-auto mb-2"
-                  />
-                  <h3 className="text-xl font-semibold">{skill.name}</h3>
-                </div>
-              ))}
+
+      <main className="max-w-7xl mx-auto mt-8 p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="bg-blue-100 p-8 rounded-lg">
+            <h2 className="text-4xl font-bold mb-2 text-gray-900">Hi I'm</h2>
+            <h1 className="text-5xl font-bold mb-4 text-gray-900">Elijah Campbell</h1>
+            <p className="mb-6 text-gray-700">
+            A forward-thinking developer with a passion for solving problems and helping others. Specializing in machine learning and artificial intelligence, I am excited to leverage today’s vast array of new technologies to build creative and useful applications
+            </p>
+            <div className="flex space-x-4">
+              <a href="/documents/Resume-Elijah-Campbell-Ihim.pdf" target="_blank" rel="noopener noreferrer">
+                <Button variant="default">View Resume</Button>
+              </a>
+              <a href="mailto:elijahcampbellihim@gmail.com" target="_blank" rel="noopener noreferrer">
+                <Button variant="ghost">Contact Me</Button>
+              </a>
+            </div>
+            <div className="flex mt-6 space-x-4">
+              <a href="https://github.com/ECampbell37" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-blue-600" title = "Github">
+                <GithubIcon size={24} />
+              </a>
+              <a href="https://linkedin.com/in/elijah-campbell-ihim" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-blue-600" title = "Linkedin">
+                <LinkedinIcon size={24} />
+              </a>
             </div>
           </div>
-        ))}
-
-        {/* Back to Home Button */}
-        <div className="mt-8">
-          <Link href="/">
-            <Button variant="link">Back to Home</Button>
-          </Link>
+          <div className="flex justify-center items-center">
+            <img
+              src="/profileAvatar.jpg"
+              alt="Elijah Campbell Avatar"
+              className="rounded-full w-72 h-72 object-cover"
+            />
+          </div>
         </div>
       </main>
+
+      <footer className="mt-16 bg-gray-100 p-4">
+        <h2 className="text-2xl font-semibold mb-4 text-center text-gray-900">Skills and Platforms</h2>
+        <ScrollArea className="w-full overflow-hidden rounded-md border">
+          <div className="scrolling-images flex space-x-1 p-4">
+            {/* Display images twice for smooth looping */}
+            {[...images, ...images].map((src, index) => (
+              <div key={index} className="flex-shrink-0">
+                <img src={src} alt={`Logo ${index + 1}`} className="w-32 h-32 object-cover rounded-md shadow-md" />
+              </div>
+            ))}
+          </div>
+        </ScrollArea>
+      </footer>
+
+      <style jsx>{`
+        .scrolling-images {
+          display: flex;
+          animation: scroll 60s linear infinite;
+          width: max-content;
+        }
+
+        @keyframes scroll {
+          0% {
+            transform: translateX(0);
+          }
+          100% {
+            transform: translateX(-50%);
+          }
+        }
+      `}</style>
     </div>
   )
 }
